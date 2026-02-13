0e88f933b185538c304ff326bc3f85688742978a
Vanita Mae Rabsatt <68659221+vanita-r@users.noreply.github.com>
2026-02-13 15:13:03 +0000

    Created bad code version file and the basic class structure

---
+++ bad_code.py
@@ -0,0 +1,5 @@
+# Basic Calculator Program - Bad Code Version
+# 3 coding principles NOT being used: KISS, Single Responsibility, and Clean Code
+
+class BadCalculator:
+    pass  # Placeholder for the class
