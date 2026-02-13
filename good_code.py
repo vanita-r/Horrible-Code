d5245e338c029b00744203c3dc28a268ac1d95bb
Vanita Mae Rabsatt <68659221+vanita-r@users.noreply.github.com>
2026-02-13 15:00:10 +0000

    Added basic calculation functions and descriptive variables; KISS and Single Responsibility

--- good_code.py
+++ good_code.py
@@ -2,4 +2,15 @@
 # 3 coding principles being used: KISS, Single Responsibility, and Clean Code
 
 class BasicCalculator:
-    pass
+    # calculations
+    def addition(self, addend1, addend2):
+        return addend1 + addend2
+    
+    def subtraction(self, minuend, subtrahend):
+        return minuend - subtrahend
+    
+    def multiplication(self, factor1, factor2):
+        return factor1 * factor2
+    
+    def division(self, dividend, divisor):
+        return dividend / divisor
